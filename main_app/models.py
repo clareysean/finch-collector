d62caaee07eae671e780b50095eefed6db96eff1
clareysean <seanpclarey@gmail.com>
2023-08-07 10:41:38 -0400

    Add detail view

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    threats = models.TextField(max_length=250)
+    habitat = models.CharField(max_length=100)
+    notes = models.TextField(max_length=250)
